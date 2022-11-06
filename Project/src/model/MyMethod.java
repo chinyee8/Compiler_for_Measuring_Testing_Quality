bf3dc16a047868e28f9bbf575678942cb85b3bf3
Grace77Han <59812158+Grace77Han@users.noreply.github.com>
2022-11-05 23:03:02 -0400

    updated containers

--- Project/src/model/MyMethod.java
+++ Project/src/model/MyMethod.java
@@ -1,5 +1,11 @@
 package model;
 
 public class MyMethod {
-
+	String methodName;
+	String methodType;
+	
+	public MyMethod(String name, String type) {
+		methodName = name;
+		methodType = type;
+	}
 }
