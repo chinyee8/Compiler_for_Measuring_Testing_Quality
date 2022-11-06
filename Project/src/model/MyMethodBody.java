8e12d2b1751b14485a1648b127e1090079fbca75
Grace77Han <59812158+Grace77Han@users.noreply.github.com>
2022-11-05 23:48:51 -0400

    added containers

--- Project/src/model/MyMethodBody.java
+++ Project/src/model/MyMethodBody.java
@@ -1,5 +1,42 @@
 package model;
 
-public class MyMethodBody {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MyMethodBody {
+	public List<Declaration> declList;
+	public List<Assignment> assiList;
+	public List<IfStatement> ifStatList;
+	public List<TestMethodCall> testMethodList;
+	
+	public MyMethodBody() {
+		declList = new ArrayList<>();
+		assiList = new ArrayList<>();
+		ifStatList = new ArrayList<>();
+		testMethodList = new ArrayList<>();
+	}
+	
+	public void addDecl(Declaration decl) {
+		
+		declList.add(decl);
+		
+	}
+	
+	public void addAssi(Assignment assi) {
+		
+		assiList.add(assi);
+		
+	}
+
+	public void addIfStatement(IfStatement ifst) {
+	
+		ifStatList.add(ifst);
+	
+	}
+	
+	public void addTestMethodCall(TestMethodCall test) {
+		
+		testMethodList.add(test);
+	
+	}
 }
