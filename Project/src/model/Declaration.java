e5d766d9d9ecab1fd4aa10987f3468cae3a98ef8
Grace77Han <59812158+Grace77Han@users.noreply.github.com>
2022-11-05 22:34:51 -0400

    added containers for varName & datatype

--- Project/src/model/Declaration.java
+++ Project/src/model/Declaration.java
@@ -1,5 +1,11 @@
 package model;
 
 public class Declaration {
-
+	public String varName;
+	public String dataType;
+	
+	public Declaration(String name, String type) { // constructor
+		varName = name;
+		dataType = type;
+	}
 }
