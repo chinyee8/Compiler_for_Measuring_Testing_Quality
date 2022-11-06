4453855a53e87c4c38696de10657ac131a896601
Grace77Han <59812158+Grace77Han@users.noreply.github.com>
2022-11-05 23:20:17 -0400

    added containers

--- Project/src/model/MyMethod.java
+++ Project/src/model/MyMethod.java
@@ -2,9 +2,19 @@ package model;
 
 public class MyMethod {
 	String methodName;
-	MyReturnM methodType;
+	
+	/* I made an abstract class to help 
+	* distinguishing method return type
+	*/
+	MethodType methodType; 
+	
 	
 	public MyMethod(String name, MyReturnM type) {
+		methodName = name;
+		methodType = type;
+	}
+	
+	public MyMethod(String name, MyVoidM type) {
 		methodName = name;
 		methodType = type;
 	}
