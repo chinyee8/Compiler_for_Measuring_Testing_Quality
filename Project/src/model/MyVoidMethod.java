829219446c5b491ddbbe6deefbf413f5bc873a0d
Grace77Han <59812158+Grace77Han@users.noreply.github.com>
2022-11-05 23:43:06 -0400

    added containers

--- Project/src/model/MyVoidMethod.java
+++ Project/src/model/MyVoidMethod.java
@@ -1,5 +1,15 @@
 package model;
 
 public class MyVoidMethod {
-
+	MyParameter param;
+	MyMethodBody body;
+	String varName;
+	
+	public MyVoidMethod(MyParameter param,
+			MyMethodBody body,
+			String varName) {
+		this.param = param;
+		this.body = body;
+		this.varName = varName;
+	}
 }
