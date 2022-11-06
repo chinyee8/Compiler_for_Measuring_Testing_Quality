ba3e8f12f9d7bb476cc60046b2130e174809bc17
Grace77Han <59812158+Grace77Han@users.noreply.github.com>
2022-11-05 22:53:27 -0400

    added containers

--- Project/src/model/AssiRMethodCall.java
+++ Project/src/model/AssiRMethodCall.java
@@ -1,5 +1,11 @@
 package model;
 
 public class AssiRMethodCall {
-
+	public String methodName;
+	public String varName;
+	
+	public AssiRMethodCall(String method, String var) {
+		methodName = method;
+		varName = var;
+	}
 }
