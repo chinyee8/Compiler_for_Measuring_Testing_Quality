7d3582cb67e399450417da4120f1d5555f776fa8
Grace77Han <59812158+Grace77Han@users.noreply.github.com>
2022-11-05 21:36:37 -0400

    added container for class

--- Project/src/model/Program.java
+++ Project/src/model/Program.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Program{
-  
-  // container such as list
+
 	public List<GameClass> games;
   
 	public Program() { // constructor
