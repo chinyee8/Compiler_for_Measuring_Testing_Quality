af9fca197a6ad414a7ca7ec263d2a836f5010aa7
Grace77Han <59812158+Grace77Han@users.noreply.github.com>
2022-11-05 21:34:26 -0400

    Update Program.java

--- Project/src/model/Program.java
+++ Project/src/model/Program.java
@@ -1,11 +1,15 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program{
   
   // container such as list
+	public List<GameClass> games;
   
-  public Program() { // constructor
-  
-  }
+	public Program() { // constructor
+		games = new ArrayList<>();
+	}
   
 }
