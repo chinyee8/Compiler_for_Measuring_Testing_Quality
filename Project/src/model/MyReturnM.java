d984a94f3acf4fb137ce808dbcce10a580d3d47b
Grace77Han <59812158+Grace77Han@users.noreply.github.com>
2022-11-05 23:31:06 -0400

    added container

--- Project/src/model/MyReturnM.java
+++ Project/src/model/MyReturnM.java
@@ -1,5 +1,9 @@
 package model;
 
-public class MyReturnM {
-
+public class MyReturnM extends MethodType{
+	public MyReturnMethod myMethod;
+	
+	public MyReturnM(MyReturnMethod method){
+		myMethod = method;
+	}
 }
