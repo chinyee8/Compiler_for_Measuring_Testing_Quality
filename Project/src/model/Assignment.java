68c8438a347efcb031eaa452a7f765358558bc4d
Grace77Han <59812158+Grace77Han@users.noreply.github.com>
2022-11-05 22:50:35 -0400

    added containers

--- Project/src/model/Assignment.java
+++ Project/src/model/Assignment.java
@@ -1,5 +1,17 @@
 package model;
 
 public class Assignment {
-
+	public String varName;
+	public AssiRMethodCall assiRMethodCall;
+	public AssiExprValues assiExprValues;
+	
+	public Assignment(String name, 
+			AssiRMethodCall method,
+			AssiExprValues expVal) {
+		
+		varName = name;
+		assiRMethodCall = method;
+		assiExprValues = expVal;
+	}
+	
 }
