4b5ddb265cd0e7183c6b195182052ea797416cd1
Grace77Han <59812158+Grace77Han@users.noreply.github.com>
2022-11-05 22:18:10 -0400

    added containers (list form)

--- Project/src/model/GameBody.java
+++ Project/src/model/GameBody.java
@@ -1,5 +1,36 @@
 package model;
 
-public class GameBody {
+import java.util.ArrayList;
+import java.util.List;
 
+public class GameBody {
+	public List<Declaration> declList;
+	public List<Assignment> assiList;
+	public List<MyMethod> myMethodList;
+	
+	public GameBody() {
+		
+		declList = new ArrayList<>();
+		assiList = new ArrayList<>();
+		myMethodList = new ArrayList<>();
+		
+	}
+	
+	public void addDecl(Declaration decl) {
+		
+		declList.add(decl);
+		
+	}
+	
+	public void addAssi(Assignment assi) {
+		
+		assiList.add(assi);
+		
+	}
+
+	public void addMyMethod(MyMethod myM) {
+	
+		myMethodList.add(myM);
+	
+	}
 }
