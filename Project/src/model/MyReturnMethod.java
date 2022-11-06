1ee1f7c4aa2ad90297bf9cabdaad84fd06f32f9e
Grace77Han <59812158+Grace77Han@users.noreply.github.com>
2022-11-05 23:41:06 -0400

    added containers

--- Project/src/model/MyReturnMethod.java
+++ Project/src/model/MyReturnMethod.java
@@ -1,5 +1,18 @@
 package model;
 
 public class MyReturnMethod {
-
+	String dataType;
+	MyParameter param;
+	MyMethodBody body;
+	String varName;
+	
+	public MyReturnMethod(String dataType,
+			MyParameter param,
+			MyMethodBody body,
+			String varName) {
+		this.dataType = dataType;
+		this.param = param;
+		this.body = body;
+		this.varName = varName;
+	}
 }
