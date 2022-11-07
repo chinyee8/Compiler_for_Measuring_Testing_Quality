37f045541465c5838317f4ee3cacbb31831a61aa
Grace77Han <59812158+Grace77Han@users.noreply.github.com>
2022-11-07 13:35:16 -0500

    AntlrToProgram is done

--- Project/src/AntlrToObject/AntlrToProgram.java
+++ Project/src/AntlrToObject/AntlrToProgram.java
@@ -10,17 +10,25 @@ import antlr.*;
 public class AntlrToProgram extends exprBaseVisitor<Program> {
 	public List<String> semanticErrors;
 	public List<Integer> linesCovered;
+	
 	@Override
 	public Program visitProgram(ProgramContext ctx) {
 		Program prog = new Program();
 		semanticErrors = new ArrayList<>();
 		
-//		AntlrToExpression exprVisitor = new AntlrToExpression(semanticErrors);
+		AntlrToGameClass gameClassVisitor = new AntlrToGameClass(semanticErrors);
+		AntlrToTestCase testCaseVisitor = new AntlrToTestCase(semanticErrors);
+		
+		int gameClassIndex = 0;
+		int testCaseIndex = 0;
+		
 		for(int i = 0; i < ctx.getChildCount(); i++) {
 			if(i == ctx.getChildCount()-1) {
 				
-			}else {
-//				prog.addExpression(exprVisitor.visit(ctx.getChild(i)));
+			}
+			else {
+				prog.addGameClass(gameClassVisitor.visit(ctx.getChild(gameClassIndex++)));
+				prog.addTestCase(testCaseVisitor.visit(ctx.getChild(testCaseIndex++)));
 			}
 		}
 		
