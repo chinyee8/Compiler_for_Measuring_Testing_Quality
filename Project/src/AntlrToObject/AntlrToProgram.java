319bba8e0aecb16785dd647f0642b6082448bb04
Grace77Han <59812158+Grace77Han@users.noreply.github.com>
2022-11-07 13:39:52 -0500

    Still thinking of it

--- Project/src/AntlrToObject/AntlrToProgram.java
+++ Project/src/AntlrToObject/AntlrToProgram.java
@@ -19,16 +19,20 @@ public class AntlrToProgram extends exprBaseVisitor<Program> {
 		AntlrToGameClass gameClassVisitor = new AntlrToGameClass(semanticErrors);
 		AntlrToTestCase testCaseVisitor = new AntlrToTestCase(semanticErrors);
 		
-		int gameClassIndex = 0;
-		int testCaseIndex = 0;
+		//int gameClassIndex = 0;
+		//int testCaseIndex = 0;
 		
 		for(int i = 0; i < ctx.getChildCount(); i++) {
 			if(i == ctx.getChildCount()-1) {
 				
 			}
 			else {
-				prog.addGameClass(gameClassVisitor.visit(ctx.getChild(gameClassIndex++)));
-				prog.addTestCase(testCaseVisitor.visit(ctx.getChild(testCaseIndex++)));
+				//kinda confused about using int i here when there are two kinds of objects to visit.
+				//should I use other 2 seperated index variables for ctx.getCHild() ??  
+				prog.addGameClass(gameClassVisitor.visit(ctx.getChild(i))); 
+				prog.addTestCase(testCaseVisitor.visit(ctx.getChild(i)));
+				//prog.addGameClass(gameClassVisitor.visit(ctx.getChild(gameClassIndex++))); 
+				//prog.addTestCase(testCaseVisitor.visit(ctx.getChild(testCaseIndex++)));
 			}
 		}
 		
