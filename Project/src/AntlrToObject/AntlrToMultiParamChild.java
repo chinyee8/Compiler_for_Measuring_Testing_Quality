25891624f95baab23777cc642f0c3b9911179d2d
Chin Yee Lai <77410638+chinyee8@users.noreply.github.com>
2022-11-07 20:30:02 -0500

    Create AntlrToMultiParamChild.java

---
+++ Project/src/AntlrToObject/AntlrToMultiParamChild.java
@@ -0,0 +1,27 @@
+package AntlrToObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import antlr.exprBaseVisitor;
+import antlr.exprParser.MultiParamChildContext;
+import model.MultiParamChild;
+import model.Params;
+
+public class AntlrToMultiParamChild extends exprBaseVisitor<MultiParamChild>{
+	public List<String> semanticErrors;
+	public List<Integer> linesCovered;
+	
+	public AntlrToMultiParamChild(List<String> semanticErrors) {
+		this.semanticErrors = new ArrayList<>();
+	}
+
+	@Override
+	public MultiParamChild visitMultiParamChild(MultiParamChildContext ctx) {
+		AntlrToParams pVisitor = new AntlrToParams(semanticErrors);
+		Params params = pVisitor.visit(ctx.getChild(0));
+		return new MultiParamChild(params);
+	}
+	
+	
+}
