1c0f536bbdf4d3db6a15aca86cddc70817aa558b
Chin Yee Lai <77410638+chinyee8@users.noreply.github.com>
2022-11-07 21:03:49 -0500

    Update AntlrToTestMethodCall.java

--- Project/src/AntlrToObject/AntlrToTestMethodCall.java
+++ Project/src/AntlrToObject/AntlrToTestMethodCall.java
@@ -1,20 +1,27 @@
 package AntlrToObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import antlr.exprBaseVisitor;
 import antlr.exprParser.TestMethodCallContext;
-import model.CallParameter;
-import model.Declaration;
+import model.Call_Parameter;
 import model.TestMethodCall;
 
 public class AntlrToTestMethodCall extends exprBaseVisitor<TestMethodCall> {
+	public List<String> semanticErrors;
+	public List<Integer> linesCovered;
+	
+	public AntlrToTestMethodCall(List<String> semanticErrors) {
+		this.semanticErrors = new ArrayList<>();
+	}
+
 
 	public TestMethodCall VisitTestMethodCall(TestMethodCallContext ctx) {
 		String className = ctx.getChild(1).getText();
 		String methodName = ctx.getChild(3).getText();
-		AntlrToCallParameter callParamVisitor = new AntlrToCallParameter();
-		CallParameter callParam = callParamVisitor.visit(ctx.getChild(4));
+		AntlrToCall_Parameter callParamVisitor = new AntlrToCall_Parameter(semanticErrors);
+		Call_Parameter callParam = callParamVisitor.visit(ctx.getChild(4));
 		return new TestMethodCall(className, methodName, callParam);
 		
 	}
