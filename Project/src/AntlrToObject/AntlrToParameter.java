66de70baabd8b2df10db839e4ffbcfded735d259
Chin Yee Lai <77410638+chinyee8@users.noreply.github.com>
2022-11-07 20:30:07 -0500

    Create AntlrToParameter.java

---
+++ Project/src/AntlrToObject/AntlrToParameter.java
@@ -0,0 +1,52 @@
+package AntlrToObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import antlr.exprBaseVisitor;
+import antlr.exprParser.EmptyParamContext;
+import antlr.exprParser.MultiParamContext;
+import antlr.exprParser.SingleParamContext;
+import model.EmptyParam;
+import model.MultiParam;
+import model.MultiParamChild;
+import model.Parameter;
+import model.Params;
+import model.SingleParam;
+
+public class AntlrToParameter extends exprBaseVisitor<Parameter>{
+	public List<String> semanticErrors;
+	public List<Integer> linesCovered;
+	
+	public AntlrToParameter(List<String> semanticErrors) {
+		this.semanticErrors = new ArrayList<>();
+	}
+
+	@Override
+	public Parameter visitSingleParam(SingleParamContext ctx) {
+		AntlrToParams pVisitor = new AntlrToParams(semanticErrors);
+		Params param = pVisitor.visit(ctx.getChild(0));
+		return new SingleParam(param);
+	}
+
+	@Override
+	public Parameter visitMultiParam(MultiParamContext ctx) {
+		AntlrToParams pVisitor = new AntlrToParams(semanticErrors);
+		Params param = pVisitor.visit(ctx.getChild(0));
+		
+		AntlrToMultiParamChild mVisitor = new AntlrToMultiParamChild(semanticErrors);
+		List<MultiParamChild> multi_param = new ArrayList<>();
+		for(int i = 1; i < ctx.getChildCount(); i++) {
+			multi_param.add(mVisitor.visit(ctx.getChild(i)));
+		}
+		
+		return new MultiParam(param, multi_param);
+	}
+
+	@Override
+	public Parameter visitEmptyParam(EmptyParamContext ctx) {
+		return new EmptyParam();
+	}
+
+	
+}
