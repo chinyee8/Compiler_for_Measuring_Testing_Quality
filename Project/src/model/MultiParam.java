221f67e043651186d6c55dd5db5e371c186e58fd
Chin Yee Lai <77410638+chinyee8@users.noreply.github.com>
2022-11-07 20:30:33 -0500

    Update MultiParam.java

--- Project/src/model/MultiParam.java
+++ Project/src/model/MultiParam.java
@@ -1,16 +1,24 @@
 package model;
 
+import java.util.List;
+
 public class MultiParam extends Parameter{
 	 Params param;
-	 MultiParamChild multi_param;
+	 List<MultiParamChild> multi_param;
 	
-	public MultiParam(Params param, MultiParamChild multi_param) {
+	public MultiParam(Params param, List<MultiParamChild> multi_param) {
 		this.param = param;
 		this.multi_param = multi_param;
 	}
 	
 	@Override
 	public String toString() {
-		return param.toString() + " " + multi_param.toString();
+		String result = "";
+		
+		for(MultiParamChild m  : multi_param) {
+			result += m.toString();
+		}
+		
+		return param.toString() + " " + result;
 	}
 }
