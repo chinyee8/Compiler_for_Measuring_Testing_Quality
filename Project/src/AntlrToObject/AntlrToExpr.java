7e66d617b407453c4430be7e200f1748e5b9c351
Chin Yee Lai <77410638+chinyee8@users.noreply.github.com>
2022-11-07 20:29:57 -0500

    Create AntlrToExpr.java

---
+++ Project/src/AntlrToObject/AntlrToExpr.java
@@ -0,0 +1,38 @@
+package AntlrToObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import antlr.exprBaseVisitor;
+import antlr.exprParser.RMethodCallContext;
+import antlr.exprParser.ValuesContext;
+import model.Expr;
+import model.RMethodCall;
+import model.ReturnMethodCall;
+import model.Values;
+
+public class AntlrToExpr extends exprBaseVisitor<Expr> {
+	public List<String> semanticErrors;
+	public List<Integer> linesCovered;
+	
+	public AntlrToExpr(List<String> semanticErrors) {
+		this.semanticErrors = new ArrayList<>();
+	}
+	
+	
+	@Override
+	public Expr visitRMethodCall(RMethodCallContext ctx) {
+		AntlrToMethodCall mVisitor = new AntlrToMethodCall(semanticErrors);
+		ReturnMethodCall r_method_call = mVisitor.visit(ctx.getChild(0)); 
+		return new RMethodCall(r_method_call);
+	}
+	
+	@Override
+	public Expr visitValues(ValuesContext ctx) {
+		AntlrToValue vVisitor = new AntlrToValue(semanticErrors);
+		Values value = vVisitor.visit(ctx.getChild(0));
+		
+				
+		return value;
+	}
+}
