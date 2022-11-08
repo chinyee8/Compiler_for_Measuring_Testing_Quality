1986bad81a3542d6df12e199f32b61d5c0864b94
Chin Yee Lai <77410638+chinyee8@users.noreply.github.com>
2022-11-07 20:30:09 -0500

    Create AntlrToParams.java

---
+++ Project/src/AntlrToObject/AntlrToParams.java
@@ -0,0 +1,26 @@
+package AntlrToObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import antlr.exprBaseVisitor;
+import antlr.exprParser.ParamsContext;
+import model.Params;
+
+public class AntlrToParams extends exprBaseVisitor<Params>{
+	public List<String> semanticErrors;
+	public List<Integer> linesCovered;
+	
+	public AntlrToParams(List<String> semanticErrors) {
+		this.semanticErrors = new ArrayList<>();
+	}
+
+	@Override
+	public Params visitParams(ParamsContext ctx) {
+		String dataType = ctx.DATA_TYPE().getText();
+		String varName = ctx.VAR_NAME().getText();
+		return new Params(dataType, varName);
+	}
+	
+	
+}
