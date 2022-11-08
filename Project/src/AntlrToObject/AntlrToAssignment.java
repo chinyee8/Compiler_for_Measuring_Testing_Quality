978a00c22ebf8a226e631a1d4a374d2b722a585e
Chin Yee Lai <77410638+chinyee8@users.noreply.github.com>
2022-11-07 20:29:46 -0500

    Create AntlrToAssignment.java

---
+++ Project/src/AntlrToObject/AntlrToAssignment.java
@@ -0,0 +1,27 @@
+package AntlrToObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import antlr.exprBaseVisitor;
+import antlr.exprParser.AssignmentContext;
+import model.Assignment;
+import model.Expr;
+
+public class AntlrToAssignment extends exprBaseVisitor<Assignment>{
+	public List<String> semanticErrors;
+	public List<Integer> linesCovered;
+	
+	public AntlrToAssignment(List<String> semanticErrors) {
+		this.semanticErrors = new ArrayList<>();
+	}
+	
+	@Override
+	public Assignment visitAssignment(AssignmentContext ctx) {
+		String varName = ctx.VAR_NAME().getText();
+		AntlrToExpr eVisitor = new AntlrToExpr(semanticErrors);
+		Expr expr = eVisitor.visit(ctx.getChild(2));
+		
+		return new Assignment(varName, expr); 
+	}
+}
