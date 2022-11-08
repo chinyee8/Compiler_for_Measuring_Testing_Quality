894038e1f5ea5bbf32dd8db266d2d4005cddaff1
Grace77Han <59812158+Grace77Han@users.noreply.github.com>
2022-11-07 20:41:07 -0500

    Update AntlrToTestMethodCall.java

--- Project/src/AntlrToObject/AntlrToTestMethodCall.java
+++ Project/src/AntlrToObject/AntlrToTestMethodCall.java
@@ -19,24 +19,3 @@ public class AntlrToTestMethodCall extends exprBaseVisitor<TestMethodCall> {
 		
 	}
 }
-package AntlrToObject;
-
-import java.util.List;
-
-import antlr.exprBaseVisitor;
-import antlr.exprParser.TestMethodCallContext;
-import model.CallParameter;
-import model.Declaration;
-import model.TestMethodCall;
-
-public class AntlrToTestMethodCall extends exprBaseVisitor<TestMethodCall> {
-
-	public TestMethodCall VisitTestMethodCall(TestMethodCallContext ctx) {
-		String className = ctx.getChild(1).getText();
-		String methodName = ctx.getChild(3).getText();
-		AntlrToCallParameter callParamVisitor = new AntlrToCallParameter();
-		CallParameter callParam = callParamVisitor.visit(ctx.getChild(4));
-		return new TestMethodCall(className, methodName, callParam);
-		
-	}
-}
