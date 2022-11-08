98270850e6b38fd4cb2e2371619e02f9bb5604d5
Chin Yee Lai <77410638+chinyee8@users.noreply.github.com>
2022-11-07 20:29:55 -0500

    Create AntlrToDeclaration.java

---
+++ Project/src/AntlrToObject/AntlrToDeclaration.java
@@ -0,0 +1,25 @@
+package AntlrToObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import antlr.exprBaseVisitor;
+import antlr.exprParser.DeclarationContext;
+import model.Declaration;
+
+public class AntlrToDeclaration extends exprBaseVisitor<Declaration>{
+	public List<String> semanticErrors;
+	public List<Integer> linesCovered;
+	
+	public AntlrToDeclaration(List<String> semanticErrors) {
+		this.semanticErrors = new ArrayList<>();
+	}
+	
+	@Override
+	public Declaration visitDeclaration(DeclarationContext ctx) {
+		String varName = ctx.VAR_NAME().getText();
+		String dataType = ctx.DATA_TYPE().getText();
+		
+		return new Declaration(varName, dataType);
+	}
+}
