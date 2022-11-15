c3dcf51ab6bed7af530a0fca88e3ece3b0c86a32
Chin Yee Lai <77410638+chinyee8@users.noreply.github.com>
2022-11-15 18:52:12 -0500

    Create Evaluator.java

---
+++ Project/src/Operations/Evaluator.java
@@ -0,0 +1,33 @@
+package Operations;
+
+import java.util.List;
+
+import model.GameClass;
+import model.TestCase;
+
+public class Evaluator implements Visitor{
+	GameClass gameclass;
+	List<TestCase> testcases;
+	
+	public Evaluator(GameClass gameclass) {
+		this.gameclass = gameclass;
+	}
+
+	public Evaluator(List<TestCase> testcases) {
+		this.testcases = testcases;
+	}
+
+	public String getEvaluations() {
+		String result = "";
+		
+			if(this.gameclass != null) {
+				result = gameclass.toString();
+			}else if(this.testcases.size() != 0){
+				for(TestCase t: testcases) {
+					result = t.toString() + "<br>";
+				}
+			}
+		
+		return result;
+	}
+}
