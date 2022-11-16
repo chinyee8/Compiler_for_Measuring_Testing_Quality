7f4d480cffc2941de12e7dd141f25a65c50ff73e
chinyee8 <laichinyee1101@gmail.com>
2022-11-16 11:27:45 -0500

    Update ExpressionApp.java

--- Project/src/app/ExpressionApp.java
+++ Project/src/app/ExpressionApp.java
@@ -32,15 +32,15 @@ public class ExpressionApp {
 				AntlrToProgram progVisitor = new AntlrToProgram();
 				Program prog = progVisitor.visit(AST);
 				if (progVisitor.semanticErrors.isEmpty()) {
-//					if(prog.gameclass != null) {
-//						Evaluator ep = new Evaluator(prog.gameclass);
-//						PrettyPrinter printer = new PrettyPrinter(ep);
-//						printer.prettyPrint();
-//					}else if(prog.testcases.size() != 0) {
-//						Evaluator ep = new Evaluator(prog.testcases);
-//						PrettyPrinter printer = new PrettyPrinter(ep);
-//						printer.prettyPrint();
-//					}
+					if(prog.gameclass != null) {
+						Evaluator ep = new Evaluator(prog.gameclass);
+						PrettyPrinter printer = new PrettyPrinter(ep);
+						printer.prettyPrint();
+					}else if(prog.testcases.size() != 0) {
+						Evaluator ep = new Evaluator(prog.testcases);
+						PrettyPrinter printer = new PrettyPrinter(ep);
+						printer.prettyPrint();
+					}
 			
 				}
 				else {
