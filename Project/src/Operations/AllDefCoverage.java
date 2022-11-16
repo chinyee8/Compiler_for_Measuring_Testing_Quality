a3357a653efdd68956a1387317fa3f5d7793d5ab
chinyee8 <laichinyee1101@gmail.com>
2022-11-16 15:10:28 -0500

    Create AllDefCoverage.java

---
+++ Project/src/Operations/AllDefCoverage.java
@@ -0,0 +1,12 @@
+package Operations;
+
+import AntlrToObject.AntlrToMathematics;
+
+public class AllDefCoverage implements Visitor{
+
+//	@Override
+//	public void visitAntlrToMathematics(AntlrToMathematics e) {
+//		
+//	}
+
+}
