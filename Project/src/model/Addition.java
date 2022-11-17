a60da0781ff269a44ff0357d1cef1da188856753
chinyee8 <laichinyee1101@gmail.com>
2022-11-17 02:26:27 -0500

    Update Addition.java

--- Project/src/model/Addition.java
+++ Project/src/model/Addition.java
@@ -1,17 +1,22 @@
 package model;
 
 public class Addition extends Mathematics{
-	Mathematics math1;
-	Mathematics math2;
+	public Mathematics math1;
+	public Mathematics math2;
 	
 	
 	public Addition(Mathematics math1, Mathematics math2) {
 		this.math1 = math1;
 		this.math2 = math2;
+		
 	}
 	
 	@Override
 	public String toString() {
 		return math1.toString() + " + " + math2.toString();
 	}
+	
+	
+	
+	
 }
