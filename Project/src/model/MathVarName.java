c3a2712c26f0203efe28b3c1947a645bd9c61634
chinyee8 <laichinyee1101@gmail.com>
2022-11-17 02:26:57 -0500

    Update MathVarName.java

--- Project/src/model/MathVarName.java
+++ Project/src/model/MathVarName.java
@@ -2,10 +2,13 @@ package model;
 
 public class MathVarName extends Mathematics{
 	String varName;
+	public Values val;
 	
-	public MathVarName(String varName) {
+	public MathVarName(String varName, Values val) {
 		this.varName = varName;
+		this.val = val;
 	}
+	
 	
 	@Override
 	public String toString() {
