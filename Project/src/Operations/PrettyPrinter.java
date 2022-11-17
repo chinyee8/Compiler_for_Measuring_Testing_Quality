cf111699b853bda9e8d9fc23a923347cf92ab1b1
Monica <monicaliu0101@gmail.com>
2022-11-17 16:26:51 -0500

    comment out prettyprinter

--- Project/src/Operations/PrettyPrinter.java
+++ Project/src/Operations/PrettyPrinter.java
@@ -13,25 +13,25 @@ public class PrettyPrinter{
 	}
 
 	public void prettyPrint( ) {
-		try {
-			File h = new File("index.html");
-			h.createNewFile();
-			//	            if (h.createNewFile()) {
-			System.out.println("File created");
-			FileWriter myWriter = new FileWriter("index.html");
-			String result = ep.getEvaluations();
-			myWriter.write("<html> <body>");
-			myWriter.write(result);
-			myWriter.write("</body> </html>");
-			myWriter.close();
-			System.out.println("successfully wrote to the file");
-			//	            } else {
-			//	                System.out.println("File already exist");
-			//	            }
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		try {
+//			File h = new File("index.html");
+//			h.createNewFile();
+//			//	            if (h.createNewFile()) {
+//			System.out.println("File created");
+//			FileWriter myWriter = new FileWriter("index.html");
+//			String result = ep.getEvaluations();
+//			myWriter.write("<html> <body>");
+//			myWriter.write(result);
+//			myWriter.write("</body> </html>");
+//			myWriter.close();
+//			System.out.println("successfully wrote to the file");
+//			//	            } else {
+//			//	                System.out.println("File already exist");
+//			//	            }
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 
 
