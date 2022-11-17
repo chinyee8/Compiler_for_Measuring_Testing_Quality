5fb730b9ede7416a33e1b3169e5e147750569e46
chinyee8 <laichinyee1101@gmail.com>
2022-11-17 18:54:04 -0500

    Update ValueBool.java

--- Project/src/model/ValueBool.java
+++ Project/src/model/ValueBool.java
@@ -16,4 +16,10 @@ public class ValueBool extends Values {
 		// TODO Auto-generated method stub
 		return "BOOLEAN";
 	}
+	
+	@Override
+	public Values getValues() {
+		// TODO Auto-generated method stub
+		return new ValueBool(value);
+	}
 }
