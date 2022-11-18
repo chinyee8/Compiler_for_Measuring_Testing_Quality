8709aed775d9e831a75b1e3691f8bd7bd94afc8a
chinyee8 <laichinyee1101@gmail.com>
2022-11-17 19:28:01 -0500

    Update AntlrToIfStatement.java

--- Project/src/AntlrToObject/AntlrToIfStatement.java
+++ Project/src/AntlrToObject/AntlrToIfStatement.java
@@ -8,14 +8,18 @@ import antlr.exprParser.IfStatementContext;
 import model.Condition;
 import model.IfStatement;
 import model.MyMethodBody;
+import model.MyMethods;
 import model.Values;
 public class AntlrToIfStatement extends exprBaseVisitor<IfStatement>  {
 	public List<String> semanticErrors;
 	public List<Integer> linesCovered;
 	public HashMap<String, Values> variableMap;
-	public AntlrToIfStatement(List<String> semanticErrors, HashMap<String, Values> variableMap) {
+	public List<MyMethods> mymethod;
+	
+	public AntlrToIfStatement(List<String> semanticErrors, HashMap<String, Values> variableMap, List<MyMethods> mymethod) {
 		this.semanticErrors = semanticErrors;
 		this.variableMap = variableMap;
+		this.mymethod = mymethod;
 	}
 	
 	@Override
@@ -24,7 +28,7 @@ public class AntlrToIfStatement extends exprBaseVisitor<IfStatement>  {
 		AntlrToCondition condVisitor = new AntlrToCondition(semanticErrors, this.variableMap);
 		Condition cond = condVisitor.visit(ctx.cond());
 		
-		AntlrToMyMethodBody BodyVisitor = new AntlrToMyMethodBody(semanticErrors,this.variableMap);
+		AntlrToMyMethodBody BodyVisitor = new AntlrToMyMethodBody(semanticErrors,this.variableMap, this.mymethod);
 
 		MyMethodBody ifBody = BodyVisitor.visit(ctx.getChild(5));
 		MyMethodBody elseBody = BodyVisitor.visit(ctx.getChild(9));
