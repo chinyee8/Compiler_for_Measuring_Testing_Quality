019da56f43ce934b07ee4f8e418ab590f062535e
chinyee8 <laichinyee1101@gmail.com>
2022-11-17 19:28:04 -0500

    Update AntlrToMethodType.java

--- Project/src/AntlrToObject/AntlrToMethodType.java
+++ Project/src/AntlrToObject/AntlrToMethodType.java
@@ -8,6 +8,7 @@ import antlr.exprParser.MyReturnMethodContext;
 import antlr.exprParser.MyVoidMethodContext;
 import model.MethodType;
 import model.MyMethodBody;
+import model.MyMethods;
 import model.MyReturnMethod;
 import model.MyVoidMethod;
 import model.Parameter;
@@ -17,11 +18,12 @@ public class AntlrToMethodType extends exprBaseVisitor<MethodType> {
 	public List<String> semanticErrors; 
 	public List<Integer> linesCovered;
 	public HashMap<String, Values> variableMap;
+	public List<MyMethods> mymethod;
 
-
-	public AntlrToMethodType(List<String> semanticError, HashMap<String, Values> variableMap) {
+	public AntlrToMethodType(List<String> semanticError, HashMap<String, Values> variableMap, List<MyMethods> mymethod) {
 		this.semanticErrors = semanticError;
 		this.variableMap = variableMap;
+		this.mymethod = mymethod;
 	}
 
 	@Override
@@ -30,7 +32,7 @@ public class AntlrToMethodType extends exprBaseVisitor<MethodType> {
 		AntlrToParameter pVisitor = new AntlrToParameter(semanticErrors);
 		Parameter parameter = pVisitor.visit(ctx.parameter());
 		
-		AntlrToMyMethodBody method_bodyVisitor = new AntlrToMyMethodBody(semanticErrors, this.variableMap);
+		AntlrToMyMethodBody method_bodyVisitor = new AntlrToMyMethodBody(semanticErrors, this.variableMap, this.mymethod);
 		MyMethodBody method_body = method_bodyVisitor.visit(ctx.method_body());
 		
 		String varName = ctx.VAR_NAME().getText();
@@ -43,7 +45,7 @@ public class AntlrToMethodType extends exprBaseVisitor<MethodType> {
 		String void_type = ctx.VOID_TYPE().getText();
 		AntlrToParameter pVisitor = new AntlrToParameter(semanticErrors);
 		Parameter parameter = pVisitor.visit(ctx.parameter());
-		AntlrToMyMethodBody method_bodyVisitor = new AntlrToMyMethodBody(semanticErrors, this.variableMap);
+		AntlrToMyMethodBody method_bodyVisitor = new AntlrToMyMethodBody(semanticErrors, this.variableMap, this.mymethod);
 		MyMethodBody method_body = method_bodyVisitor.visit(ctx.method_body());
 		return new MyVoidMethod(void_type, parameter, method_body);
 	}
