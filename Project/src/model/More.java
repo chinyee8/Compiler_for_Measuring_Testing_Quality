91b4d5eacce596b64ad2a756f507392d4503f948
chinyee8 <laichinyee1101@gmail.com>
2022-11-18 12:03:13 -0500

    Update More.java

--- Project/src/model/More.java
+++ Project/src/model/More.java
@@ -1,8 +1,8 @@
 package model;
 
 public class More extends Condition{
-	Mathematics math1;
-	Mathematics math2;
+	public Mathematics math1;
+	public Mathematics math2;
 	
 	public More(Mathematics math1, Mathematics math2) {
 		this.math1 = math1;
