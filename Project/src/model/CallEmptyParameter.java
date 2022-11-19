b54a2a541026b8d8827b7a27506efbaa714f22fd
chinyee8 <laichinyee1101@gmail.com>
2022-11-19 15:35:12 -0500

    Update CallEmptyParameter.java

--- Project/src/model/CallEmptyParameter.java
+++ Project/src/model/CallEmptyParameter.java
@@ -1,8 +1,19 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CallEmptyParameter extends Call_Parameter {
 	@Override
 	public String toString() {
 		return "";
+	}
+	
+	public List<String> getCallParams(){
+		List<String> callparams = new ArrayList<>();
+		
+		
+		
+		return callparams;
 	}
 }
