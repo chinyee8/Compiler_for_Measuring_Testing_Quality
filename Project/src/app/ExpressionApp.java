e95a895bc9fe500988cdb13832036a61441e78a8
chinyee8 <laichinyee1101@gmail.com>
2022-11-24 13:00:10 -0500

    Update ExpressionApp.java

--- Project/src/app/ExpressionApp.java
+++ Project/src/app/ExpressionApp.java
@@ -66,13 +66,18 @@ public class ExpressionApp {
 						Map<Integer, List<Integer>> useLines = new HashMap<>();
 						Map<Integer, List<String>> lines = new HashMap<>();
 						Map<Integer, Integer> defpercentage = new HashMap<>();
-
+						List<String> errors = new ArrayList<>();
+						boolean containErrors = false;
 
 						for(Map.Entry<MethodCall, Map<String, Values>> t : testProg.testcase.allMethodCalls.entrySet()) {
 
 							AntlrToProgram progControllor = new AntlrToProgram(t.getKey(), t.getValue(), testProg.testcase.methodCallParamOrder.get(t.getKey())); //pass in methodcall, input parameters, and order of input parameters
 
 							Program prog2 = progControllor.control((ProgramContext)progAST);
+							if(progControllor.semanticErrors.size()>0) {
+								containErrors = true;
+								
+							}
 							programList.add(prog2);
 						}
 
@@ -84,6 +89,10 @@ public class ExpressionApp {
 
 							AntlrToProgram devCoverage = new AntlrToProgram(t.getKey(), t.getValue());
 							Program defProg = devCoverage.defControl((ProgramContext)progAST);
+							if(devCoverage.semanticErrors.size()>0) {
+								containErrors = true;
+								errors.addAll(devCoverage.semanticErrors);
+							}
 
 							List<Integer> key = new ArrayList<>();
 							List<Integer> value = new ArrayList<>();
@@ -106,41 +115,48 @@ public class ExpressionApp {
 							i++;
 						}
 
-						// Condition Coverage
-						ConditionCoverage condCov = new ConditionCoverage();
+						if(containErrors) {
+							for(String err: errors) {
+								System.out.println(err);
+							}
+						}else {
+							// Condition Coverage
+							ConditionCoverage condCov = new ConditionCoverage();
 
-						AntlrToProgram progCondComp = new AntlrToProgram(condCov);
-						progCondComp.visitConditionCoverage((ProgramContext)progAST); // for addCompoenent
+							AntlrToProgram progCondComp = new AntlrToProgram(condCov);
+							progCondComp.visitConditionCoverage((ProgramContext)progAST); // for addCompoenent
 
-						condCov.setComponentState(false); // now adding result state
-						
-						//temp
-						//condCov.appendResultString("0");
-						//condCov.addResult();
-						
-						for(Map.Entry<MethodCall, Map<String, Values>> t : testProg.testcase.allMethodCalls.entrySet()) {
-							//AntlrToProgram progCond = new AntlrToProgram(t.getKey(), t.getValue(), condCov);
-							condCov.setTestMethod(t);
-							AntlrToProgram progCond = new AntlrToProgram(condCov);
-							progCond.visitConditionCoverage((ProgramContext)progAST);	 // for addResult						
+							condCov.setComponentState(false); // now adding result state
+							
+							//temp
+							//condCov.appendResultString("0");
+							//condCov.addResult();
+							
+							for(Map.Entry<MethodCall, Map<String, Values>> t : testProg.testcase.allMethodCalls.entrySet()) {
+								//AntlrToProgram progCond = new AntlrToProgram(t.getKey(), t.getValue(), condCov);
+								condCov.setTestMethod(t);
+								AntlrToProgram progCond = new AntlrToProgram(condCov);
+								progCond.visitConditionCoverage((ProgramContext)progAST);	 // for addResult						
+							}
+							
+							Evaluator ep = new Evaluator(testProg.testcase, prog.gameclass);
+
+							Statement st = new Statement(programList, lines);
+
+							
+
+							AllDefCoverage alldef = new AllDefCoverage(defLines, useLines, lines, defpercentage);
+							AllCUsesCoverage allc = new AllCUsesCoverage(defLines, useLines, lines, defpercentage);
+
+							PrettyPrinter printer = new PrettyPrinter(ep, lines, i);
+							printer.addStatement(st);
+							printer.addAllDefCoverage(alldef);
+							printer.addAllCUseCoverage(allc);
+							printer.addCondCoverage(condCov);
+
+							printer.prettyPrint();
 						}
 						
-						Evaluator ep = new Evaluator(testProg.testcase, prog.gameclass);
-
-						Statement st = new Statement(programList, lines);
-
-						
-
-						AllDefCoverage alldef = new AllDefCoverage(defLines, useLines, lines, defpercentage);
-						AllCUsesCoverage allc = new AllCUsesCoverage(defLines, useLines, lines, defpercentage);
-
-						PrettyPrinter printer = new PrettyPrinter(ep, lines, i);
-						printer.addStatement(st);
-						printer.addAllDefCoverage(alldef);
-						printer.addAllCUseCoverage(allc);
-						printer.addCondCoverage(condCov);
-
-						printer.prettyPrint();
 					}else {
 						System.err.println("Error: please input game file first, before test file!");
 					}
