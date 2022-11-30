c6f07259dfff808617fd4b9ed6813a162a8a2bfb
chinyee8 <laichinyee1101@gmail.com>
2022-11-30 16:53:24 -0500

    Update AntlrToGameClass.java

--- Project/src/AntlrToObject/AntlrToGameClass.java
+++ Project/src/AntlrToObject/AntlrToGameClass.java
@@ -14,12 +14,14 @@ import antlr.exprParser.GameClassContext;
 import model.GameBody;
 import model.GameClass;
 import model.MethodCall;
+import model.MyMethods;
 import model.TestMethodCall;
 import model.Values;
 
 public class AntlrToGameClass extends exprBaseVisitor<GameClass>{
 	public List<String> semanticErrors; 
-	
+	public List<MyMethods> global_methods;
+
 	//controller fields
 	public ArrayList<String>[] tokensMappedToLines; //index of array + 1 correspond to line number in program 
 	public int[] rangeOfLines;
@@ -30,25 +32,21 @@ public class AntlrToGameClass extends exprBaseVisitor<GameClass>{
 	public List<String> methodCallParamOrder;
 
 	//CY's def_coverage
-	public Map<String, Boolean> def; 
-	public Map<Map<Integer, Map<String, Boolean>>, List<Integer>>  def_use;
-	public Map<Integer, Map<String, Boolean>> linesDef;
-	public List<Integer> linesUse;
+	public Map<String, String> def; 
 	public List<String> lines;
 	public int totalNotUsed;
 	
 	// Condition Coverage member variable
 	public ConditionCoverage condCov;
+
 	
-	public AntlrToGameClass() {
-		
-	}
 	public AntlrToGameClass(ArrayList<String>[] tokenLines, ArrayList<Integer> order) {
 		this.tokensMappedToLines = tokenLines;
 		this.orderOfFlow = order;
 	}
-	public AntlrToGameClass(List<String> semanticError) {
+	public AntlrToGameClass(List<String> semanticError, List<MyMethods> global_methods) {
 		this.semanticErrors = semanticError;
+		this.global_methods = global_methods;
 	}
 	
 	public AntlrToGameClass(List<String> semanticError, MethodCall t_method_call2, Map<String, Values> inputValues, List<String> methodCallParamOrder) {
@@ -59,14 +57,12 @@ public class AntlrToGameClass extends exprBaseVisitor<GameClass>{
 	}
 	
 	//CY's def_coverage
-	public AntlrToGameClass(List<String> semanticErrors,MethodCall t_method_call, Map<String, Values> inputValues, Map<String, Boolean> def, Map<Map<Integer, Map<String, Boolean>>, List<Integer>>  def_use,Map<Integer, Map<String, Boolean>> linesDef, List<Integer> linesUse, List<String> lines, int totalNotUsed) {
+	public AntlrToGameClass(List<String> semanticErrors,MethodCall t_method_call, Map<String, Values> inputValues, List<MyMethods> global_methods, Map<String, String> def, List<String> lines, int totalNotUsed) {
 		this.semanticErrors = semanticErrors;
 		this.t_method_call = t_method_call;
 		this.inputValues = inputValues;
+		this.global_methods = global_methods;
 		this.def = def;
-		this.def_use = def_use;
-		this.linesDef = linesDef;
-		this.linesUse = linesUse;
 		this.lines = lines;
 		this.totalNotUsed = totalNotUsed;
 	}
@@ -83,7 +79,7 @@ public class AntlrToGameClass extends exprBaseVisitor<GameClass>{
 	@Override
 	public GameClass visitGameClass(GameClassContext ctx) {
 		String className = ctx.CLASS_NAME().getText();
-		AntlrToGameBody gbVisitor = new AntlrToGameBody(semanticErrors);
+		AntlrToGameBody gbVisitor = new AntlrToGameBody(semanticErrors, global_methods);
 		GameBody gamebody = gbVisitor.visit(ctx.body());
 		return new GameClass(className, gamebody);
 	}
@@ -116,11 +112,9 @@ public class AntlrToGameClass extends exprBaseVisitor<GameClass>{
 	//defCoverage
 	public GameClass defControl(GameClassContext ctx) {
 		String className = ctx.CLASS_NAME().getText();
-		lines.add("game " + className + " [] !");
-		AntlrToGameBody gbVisitor = new AntlrToGameBody(semanticErrors, t_method_call, inputValues, def, def_use, linesDef, linesUse, lines, totalNotUsed);
+		AntlrToGameBody gbVisitor = new AntlrToGameBody(semanticErrors, t_method_call, inputValues, global_methods, def, lines, totalNotUsed);
 		GameBody gamebody = gbVisitor.defControl((GameBodyContext) ctx.body());
 		totalNotUsed = gbVisitor.totalNotUsed;
-		lines.add("!");
 		return new GameClass(className, gamebody);
 	}
 	
