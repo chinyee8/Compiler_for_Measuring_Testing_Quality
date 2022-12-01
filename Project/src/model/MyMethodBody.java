f8483810c640b86a3da955f9cfebbfdd9de519eb
chinyee8 <laichinyee1101@gmail.com>
2022-11-30 20:36:47 -0500

    Update MyMethodBody.java

--- Project/src/model/MyMethodBody.java
+++ Project/src/model/MyMethodBody.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -27,7 +28,9 @@ public class MyMethodBody{
 		this.loops = loops;
 	}
 
-	public void getDeclaredList() {
+	public void getDeclaredList(Map<String, Values> vars2) {
+		vars.putAll(vars2);
+		
 		for(Declaration d: declList) {
 			if(!vars.containsKey(d.varName)) {
 				vars.put(d.varName, d.defaultValue);
@@ -52,10 +55,26 @@ public class MyMethodBody{
 				vars.put(a.varName, callExpr(((ReturnMethodCall)a.expr), a.varName));
 			}
 		}
-//		for(IfStatement i : ifStatList) {
-//			MyMethodBody ifbody = i.getIfBody(vars).get(0);
-//			vars.putAll(ifbody.getValues(vars));
-//		}
+
+		for(IfStatement ifs: ifStatList) {
+			MyMethodBody ifBody = ifs.ifBody;
+			MyMethodBody elseBody = ifs.elseBody;
+
+
+			List<String> convarlist = new ArrayList<>();
+			convarlist = getCondVariableList(ifs.cond, convarlist);
+
+			ifs.setCond(evaluated(ifs.cond, vars));
+			
+			if(evaluated(ifs.cond, vars)) {
+				ifBody.getDeclaredList(vars2);
+				this.vars.putAll(ifBody.vars);
+			}else {
+				elseBody.getDeclaredList(vars2);
+				this.vars.putAll(elseBody.vars);
+			}
+		}
+
 	}
 
 	@Override
@@ -84,7 +103,7 @@ public class MyMethodBody{
 
 	public Map<String, Values> getValues(Map<String, Values> lists) {
 		this.vars.putAll(lists);
-		getDeclaredList();
+		getDeclaredList(this.vars);
 
 		return this.vars;
 	}
@@ -177,7 +196,7 @@ public class MyMethodBody{
 			result = "DOUBLE";
 		}else if(m instanceof MathVarName) {
 			MathVarName a = (MathVarName) m;
-			result = a.val.getType();
+			result = this.vars.get(a.varName).getType();
 		}
 
 		return result;
@@ -213,7 +232,7 @@ public class MyMethodBody{
 			result = a.num;
 		}else if(m instanceof MathVarName) {
 			MathVarName a = (MathVarName) m;
-			if(a.val.getType().equals("DOUBLE")) {
+			if(this.vars.get(a.varName).getType().equals("DOUBLE")) {
 				result = ((ValueDouble)(vars.get(a.varName))).value;
 			}
 		}
@@ -254,6 +273,209 @@ public class MyMethodBody{
 			MathVarName a = (MathVarName) m;
 			if(a.val.getType().equals("INT")) {
 				result = ((ValueNum)(vars.get(a.varName))).num;
+			}
+		}
+
+		return result;
+	}
+	
+	private List<String> getCondVariableList(Condition c, List<String> list) {
+		if(c instanceof Negation) {
+			Negation e = (Negation) c;
+			list = getCondVariableList(e.cond, list);
+		}else if(c instanceof CondParenthesis) {
+			CondParenthesis e = (CondParenthesis) c;
+			list = getCondVariableList(e.cond, list);
+		}else if(c instanceof Conjunction) {
+			Conjunction e = (Conjunction) c;
+			list = getCondVariableList(e.cond1, list);
+			list = getCondVariableList(e.cond2, list);
+		}else if(c instanceof Disjunction) {
+			Disjunction e = (Disjunction) c;
+			list = getCondVariableList(e.cond1, list);
+			list = getCondVariableList(e.cond2, list);
+		}else if(c instanceof EqualTo) {
+			EqualTo e = (EqualTo) c;
+			list = getCondVariableList(e.cond1, list);
+			list = getCondVariableList(e.cond2, list);
+		}else if(c instanceof NotEqualTo) {
+			NotEqualTo e = (NotEqualTo) c;
+			list = getCondVariableList(e.left, list);
+			list = getCondVariableList(e.right, list);
+		}else if(c instanceof CondBool) {
+			CondBool e = (CondBool) c;
+		}else if(c instanceof CondEqual) {
+			CondEqual e = (CondEqual) c;
+			list = getVariables(e.math1, list);
+			list = getVariables(e.math2, list);
+		}else if(c instanceof CondNotEqual) {
+			CondNotEqual e = (CondNotEqual) c;
+			list = getVariables(e.math1, list);
+			list = getVariables(e.math2, list);
+		}else if(c instanceof MoreOrEqual) {
+			MoreOrEqual e = (MoreOrEqual) c;
+			list = getVariables(e.math1, list);
+			list = getVariables(e.math2, list);
+		}else if(c instanceof LessOrEqual) {
+			LessOrEqual e = (LessOrEqual) c;
+			list = getVariables(e.left, list);
+			list = getVariables(e.right, list);
+		}else if(c instanceof More) {
+			More e = (More) c;
+			list = getVariables(e.math1, list);
+			list = getVariables(e.math2, list);
+		}else if(c instanceof Less) {
+			Less e = (Less) c;
+			list = getVariables(e.math1, list);
+			list = getVariables(e.math2, list);
+		}else if(c instanceof CondVarName) {
+			CondVarName e = (CondVarName) c;
+			if(!list.contains(e.varName)) {
+				list.add(e.varName);
+			}
+		}
+		return list;
+	}
+	
+	private List<String> getVariables(Mathematics m, List<String> list) {
+		if(m instanceof Addition) {
+			Addition a = (Addition) m;
+			list= getVariables(a.math1, list);
+			list = getVariables(a.math2, list);
+		}else if(m instanceof Subtraction) {
+			Subtraction a = (Subtraction) m;
+			list = getVariables(a.math1, list);
+			list = getVariables(a.math2, list);
+		}else if(m instanceof Multiplication) {
+			Multiplication a = (Multiplication) m;
+			list = getVariables(a.math1, list);
+			list = getVariables(a.math2, list);
+		}else if(m instanceof Division) {
+			Division a = (Division) m;
+			list = getVariables(a.math1, list);
+			list = getVariables(a.math2, list);
+		}else if(m instanceof MathParenthesis) {
+			MathParenthesis a = (MathParenthesis) m;
+		}else if(m instanceof MathDouble) {
+			MathDouble a = (MathDouble) m;
+		}else if(m instanceof MathNumber) {
+			MathNumber a = (MathNumber) m;
+		}else if(m instanceof MathVarName) {
+			MathVarName a = (MathVarName) m;
+			if(!list.contains(a.varName)) {
+				list.add(a.varName);
+			}
+		}
+
+		return list;
+	}
+	
+	public boolean evaluated(Condition c, Map<String, Values> map) {
+		boolean result = false;
+
+		if(c instanceof Negation) {
+			Negation e = (Negation) c;
+			result = !(evaluated(e.cond, map));
+
+		}else if(c instanceof CondParenthesis) {
+			CondParenthesis e = (CondParenthesis) c;
+			result = evaluated(e.cond, map);
+		}else if(c instanceof Conjunction) {
+			Conjunction e = (Conjunction) c;
+			boolean left = evaluated(e.cond1, map);
+			boolean right = evaluated(e.cond2, map);
+			result = left && right;
+		}else if(c instanceof Disjunction) {
+			Disjunction e = (Disjunction) c;
+			boolean left = evaluated(e.cond1, map);
+			boolean right = evaluated(e.cond2, map);
+			result = left || right;
+		}else if(c instanceof EqualTo) {
+			EqualTo e = (EqualTo) c;
+			boolean left = evaluated(e.cond1, map);
+			boolean right = evaluated(e.cond2, map);
+			result = left == right;
+		}else if(c instanceof NotEqualTo) {
+			NotEqualTo e = (NotEqualTo) c;
+			boolean left = evaluated(e.left, map);
+			boolean right = evaluated(e.right, map);
+			result = left != right;
+		}else if(c instanceof CondBool) {
+			CondBool e = (CondBool) c;
+			result = e.bool;
+		}else if(c instanceof CondEqual) {
+			CondEqual e = (CondEqual) c;
+			if(getMATHTYPE(e.math1).equals("INT")) {
+				int left = getInt(e.math1);
+				int right = getInt(e.math2);
+				result = left == right;
+			}else if(getMATHTYPE(e.math1).equals("DOUBLE")) {
+				double left = getDouble(e.math1);
+				double right = getDouble(e.math2);
+				result = left == right;
+			}
+		}else if(c instanceof CondNotEqual) {
+			CondNotEqual e = (CondNotEqual) c;
+			if(getMATHTYPE(e.math1).equals("INT")) {
+				int left = getInt(e.math1);
+				int right = getInt(e.math2);
+				result = left != right;
+			}else if(getMATHTYPE(e.math1).equals("DOUBLE")) {
+				double left = getDouble(e.math1);
+				double right = getDouble(e.math2);
+				result = left != right;
+			}
+		}else if(c instanceof MoreOrEqual) {
+			MoreOrEqual e = (MoreOrEqual) c;
+			if(getMATHTYPE(e.math1).equals("INT")) {
+				int left = getInt(e.math1);
+				int right = getInt(e.math2);
+				result = left >= right;
+			}else if(getMATHTYPE(e.math1).equals("DOUBLE")) {
+				double left = getDouble(e.math1);
+				double right = getDouble(e.math2);
+				result = left >= right;
+			}
+		}else if(c instanceof LessOrEqual) {
+			LessOrEqual e = (LessOrEqual) c;
+			if(getMATHTYPE(e.left).equals("INT")) {
+				int left = getInt(e.left);
+				int right = getInt(e.right);
+				result = left <= right;
+			}else if(getMATHTYPE(e.left).equals("DOUBLE")) {
+				double left = getDouble(e.left);
+				double right = getDouble(e.right);
+				result = left <= right;
+			}
+		}else if(c instanceof More) {
+			More e = (More) c;
+			if(getMATHTYPE(e.math1).equals("INT")) {
+				int left = getInt(e.math1);
+				int right = getInt(e.math2);
+				result = left > right;
+			}else if(getMATHTYPE(e.math1).equals("DOUBLE")) {
+				double left = getDouble(e.math1);
+				double right = getDouble(e.math2);
+				result = left > right;
+			}
+		}else if(c instanceof Less) {
+			Less e = (Less) c;
+			if(getMATHTYPE(e.math1).equals("INT")) {
+				int left = getInt(e.math1);
+				int right = getInt(e.math2);
+				result = left < right;
+			}else if(getMATHTYPE(e.math1).equals("DOUBLE")) {
+				double left = getDouble(e.math1);
+				double right = getDouble(e.math2);
+				result = left < right;
+			}
+		}else if(c instanceof CondVarName) {
+			CondVarName e = (CondVarName) c;
+			if(map.containsKey(e.varName)) {
+				Values val = map.get(e.varName);
+				if(val.getType().equals("BOOLEAN")) {
+					result = ((ValueBool)val.getValues()).value;
+				}
 			}
 		}
 
