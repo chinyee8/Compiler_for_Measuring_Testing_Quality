53e2887c24771ac805c139db5e59381832ad5fc7
chinyee8 <laichinyee1101@gmail.com>
2022-12-03 20:15:40 -0500

    Update CallParameter.java

--- Project/src/model/CallParameter.java
+++ Project/src/model/CallParameter.java
@@ -5,21 +5,29 @@ import java.util.List;
 
 public class CallParameter extends Call_Parameter {
 	Input_List input;
-	
+
 	public CallParameter(Input_List input) {
 		this.input = input;
 	}
-	
+
 	@Override
 	public String toString() {
 		return input.toString();
 	}
-	
+
 	public List<String> getCallParams(){
 		List<String> callparams = new ArrayList<>();
-		
+
 		callparams.add(((CallParamVarName)input).varName);
-		
+
+		return callparams;
+	}
+
+	public List<Input_List> getTestCallParams() {
+		List<Input_List> callparams = new ArrayList<>();
+
+		callparams.add(input);
+
 		return callparams;
 	}
 }
