782eb5c551cc57c7ad52e447d2d40fa7e24eb2fa
chinyee8 <laichinyee1101@gmail.com>
2022-12-03 23:50:21 -0500

    Update MyMethodBody.java

--- Project/src/model/MyMethodBody.java
+++ Project/src/model/MyMethodBody.java
@@ -28,7 +28,7 @@ public class MyMethodBody{
 		this.loops = loops;
 	}
 
-	public void getDeclaredList(Map<String, Values> vars2) {
+	public void getDeclaredList(Map<String, Values> vars2, Parameter parameter) {
 		vars.putAll(vars2);
 
 		for(Declaration d: declList) {
@@ -42,10 +42,10 @@ public class MyMethodBody{
 				if(((Values)a.expr) instanceof ValueMath) {
 					String type = getMATHTYPE(((ValueMath)a.expr).math);
 					if(type == "DOUBLE") {
-						double d = getDouble(((ValueMath)a.expr).math);
+						double d = getDouble(((ValueMath)a.expr).math, parameter);
 						vars.put(a.varName, new ValueDouble(d));
 					}else if(type == "INT") {
-						int i = getInt(((ValueMath)a.expr).math);
+						int i = getInt(((ValueMath)a.expr).math, parameter);
 						vars.put(a.varName, new ValueNum(i));
 					}
 				}else {
@@ -64,13 +64,13 @@ public class MyMethodBody{
 			List<String> convarlist = new ArrayList<>();
 			convarlist = getCondVariableList(ifs.cond, convarlist);
 
-			ifs.setCond(evaluated(ifs.cond, vars));
+			ifs.setCond(evaluated(ifs.cond, vars, parameter));
 
-			if(evaluated(ifs.cond, vars)) {
-				ifBody.getDeclaredList(vars2);
+			if(evaluated(ifs.cond, vars, parameter)) {
+				ifBody.getDeclaredList(vars2, parameter);
 				this.vars.putAll(ifBody.vars);
 			}else {
-				elseBody.getDeclaredList(vars2);
+				elseBody.getDeclaredList(vars2, parameter);
 				this.vars.putAll(elseBody.vars);
 			}
 		}
@@ -113,7 +113,7 @@ public class MyMethodBody{
 			}
 			j++;
 		}
-		getDeclaredList(this.vars);
+		getDeclaredList(this.vars, parameter);
 
 		return this.vars;
 	}
@@ -194,37 +194,37 @@ public class MyMethodBody{
 		return result;
 	}
 
-	private double getDouble(Mathematics m) {
+	private double getDouble(Mathematics m, Parameter parameter) {
 		double result = 0.00;
 		if(m instanceof Addition) {
 			Addition a = (Addition) m;
-			double left = getDouble(a.math1);
-			double right = getDouble(a.math2);
+			double left = getDouble(a.math1, parameter);
+			double right = getDouble(a.math2, parameter);
 			result = left + right;
 		}else if(m instanceof Subtraction) {
 			Subtraction a = (Subtraction) m;
-			double left = getDouble(a.math1);
-			double right = getDouble(a.math2);
+			double left = getDouble(a.math1, parameter);
+			double right = getDouble(a.math2, parameter);
 			result = left - right;
 		}else if(m instanceof Multiplication) {
 			Multiplication a = (Multiplication) m;
-			double left = getDouble(a.math1);
-			double right = getDouble(a.math2);
+			double left = getDouble(a.math1, parameter);
+			double right = getDouble(a.math2, parameter);
 			result = left * right;
 		}else if(m instanceof Division) {
 			Division a = (Division) m;
-			double left = getDouble(a.math1);
-			double right = getDouble(a.math2);
+			double left = getDouble(a.math1, parameter);
+			double right = getDouble(a.math2, parameter);
 			result = left / right;
 		}else if(m instanceof MathParenthesis) {
 			MathParenthesis a = (MathParenthesis) m;
-			result = getDouble(a.math);
+			result = getDouble(a.math, parameter);
 		}else if(m instanceof MathDouble) {
 			MathDouble a = (MathDouble) m;
 			result = a.num;
 		}else if(m instanceof MathVarName) {
 			MathVarName a = (MathVarName) m;
-			if(this.vars.get(a.varName).getType().equals("DOUBLE")) {
+			if(parameter.getParams().get(a.varName).equals("DOUBLE")) {
 				result = ((ValueDouble)(vars.get(a.varName))).value;
 			}
 		}
@@ -232,38 +232,38 @@ public class MyMethodBody{
 		return result;
 	}
 
-	public int getInt(Mathematics m) {
+	public int getInt(Mathematics m, Parameter parameter) {
 		int result = 0;
 
 		if(m instanceof Addition) {
 			Addition a = (Addition) m;
-			int left = getInt(a.math1);
-			int right = getInt(a.math2);
+			int left = getInt(a.math1, parameter);
+			int right = getInt(a.math2, parameter);
 			result = left + right;
 		}else if(m instanceof Subtraction) {
 			Subtraction a = (Subtraction) m;
-			int left = getInt(a.math1);
-			int right = getInt(a.math2);
+			int left = getInt(a.math1, parameter);
+			int right = getInt(a.math2, parameter);
 			result = left - right;
 		}else if(m instanceof Multiplication) {
 			Multiplication a = (Multiplication) m;
-			int left = getInt(a.math1);
-			int right = getInt(a.math2);
+			int left = getInt(a.math1, parameter);
+			int right = getInt(a.math2, parameter);
 			result = left * right;
 		}else if(m instanceof Division) {
 			Division a = (Division) m;
-			int left = getInt(a.math1);
-			int right = getInt(a.math2);
+			int left = getInt(a.math1, parameter);
+			int right = getInt(a.math2, parameter);
 			result = left / right;
 		}else if(m instanceof MathParenthesis) {
 			MathParenthesis a = (MathParenthesis) m;
-			result = getInt(a.math);
+			result = getInt(a.math, parameter);
 		}else if(m instanceof MathNumber) {
 			MathNumber a = (MathNumber) m;
 			result = a.num;
 		}else if(m instanceof MathVarName) {
 			MathVarName a = (MathVarName) m;
-			if(a.val.getType().equals("INT")) {
+			if(parameter.getParams().get(a.varName).equals("INT")) {
 				result = ((ValueNum)(vars.get(a.varName))).num;
 			}
 		}
@@ -362,35 +362,35 @@ public class MyMethodBody{
 		return list;
 	}
 
-	public boolean evaluated(Condition c, Map<String, Values> map) {
+	public boolean evaluated(Condition c, Map<String, Values> map, Parameter parameter) {
 		boolean result = false;
 
 		if(c instanceof Negation) {
 			Negation e = (Negation) c;
-			result = !(evaluated(e.cond, map));
+			result = !(evaluated(e.cond, map, parameter));
 
 		}else if(c instanceof CondParenthesis) {
 			CondParenthesis e = (CondParenthesis) c;
-			result = evaluated(e.cond, map);
+			result = evaluated(e.cond, map, parameter);
 		}else if(c instanceof Conjunction) {
 			Conjunction e = (Conjunction) c;
-			boolean left = evaluated(e.cond1, map);
-			boolean right = evaluated(e.cond2, map);
+			boolean left = evaluated(e.cond1, map, parameter);
+			boolean right = evaluated(e.cond2, map, parameter);
 			result = left && right;
 		}else if(c instanceof Disjunction) {
 			Disjunction e = (Disjunction) c;
-			boolean left = evaluated(e.cond1, map);
-			boolean right = evaluated(e.cond2, map);
+			boolean left = evaluated(e.cond1, map, parameter);
+			boolean right = evaluated(e.cond2, map, parameter);
 			result = left || right;
 		}else if(c instanceof EqualTo) {
 			EqualTo e = (EqualTo) c;
-			boolean left = evaluated(e.cond1, map);
-			boolean right = evaluated(e.cond2, map);
+			boolean left = evaluated(e.cond1, map, parameter);
+			boolean right = evaluated(e.cond2, map, parameter);
 			result = left == right;
 		}else if(c instanceof NotEqualTo) {
 			NotEqualTo e = (NotEqualTo) c;
-			boolean left = evaluated(e.left, map);
-			boolean right = evaluated(e.right, map);
+			boolean left = evaluated(e.left, map, parameter);
+			boolean right = evaluated(e.right, map, parameter);
 			result = left != right;
 		}else if(c instanceof CondBool) {
 			CondBool e = (CondBool) c;
@@ -398,67 +398,67 @@ public class MyMethodBody{
 		}else if(c instanceof CondEqual) {
 			CondEqual e = (CondEqual) c;
 			if(getMATHTYPE(e.math1).equals("INT")) {
-				int left = getInt(e.math1);
-				int right = getInt(e.math2);
+				int left = getInt(e.math1, parameter);
+				int right = getInt(e.math2, parameter);
 				result = left == right;
 			}else if(getMATHTYPE(e.math1).equals("DOUBLE")) {
-				double left = getDouble(e.math1);
-				double right = getDouble(e.math2);
+				double left = getDouble(e.math1, parameter);
+				double right = getDouble(e.math2, parameter);
 				result = left == right;
 			}
 		}else if(c instanceof CondNotEqual) {
 			CondNotEqual e = (CondNotEqual) c;
 			if(getMATHTYPE(e.math1).equals("INT")) {
-				int left = getInt(e.math1);
-				int right = getInt(e.math2);
+				int left = getInt(e.math1, parameter);
+				int right = getInt(e.math2, parameter);
 				result = left != right;
 			}else if(getMATHTYPE(e.math1).equals("DOUBLE")) {
-				double left = getDouble(e.math1);
-				double right = getDouble(e.math2);
+				double left = getDouble(e.math1, parameter);
+				double right = getDouble(e.math2, parameter);
 				result = left != right;
 			}
 		}else if(c instanceof MoreOrEqual) {
 			MoreOrEqual e = (MoreOrEqual) c;
 			if(getMATHTYPE(e.math1).equals("INT")) {
-				int left = getInt(e.math1);
-				int right = getInt(e.math2);
+				int left = getInt(e.math1, parameter);
+				int right = getInt(e.math2, parameter);
 				result = left >= right;
 			}else if(getMATHTYPE(e.math1).equals("DOUBLE")) {
-				double left = getDouble(e.math1);
-				double right = getDouble(e.math2);
+				double left = getDouble(e.math1, parameter);
+				double right = getDouble(e.math2, parameter);
 				result = left >= right;
 			}
 		}else if(c instanceof LessOrEqual) {
 			LessOrEqual e = (LessOrEqual) c;
 			if(getMATHTYPE(e.left).equals("INT")) {
-				int left = getInt(e.left);
-				int right = getInt(e.right);
+				int left = getInt(e.left, parameter);
+				int right = getInt(e.right, parameter);
 				result = left <= right;
 			}else if(getMATHTYPE(e.left).equals("DOUBLE")) {
-				double left = getDouble(e.left);
-				double right = getDouble(e.right);
+				double left = getDouble(e.left, parameter);
+				double right = getDouble(e.right, parameter);
 				result = left <= right;
 			}
 		}else if(c instanceof More) {
 			More e = (More) c;
 			if(getMATHTYPE(e.math1).equals("INT")) {
-				int left = getInt(e.math1);
-				int right = getInt(e.math2);
+				int left = getInt(e.math1, parameter);
+				int right = getInt(e.math2, parameter);
 				result = left > right;
 			}else if(getMATHTYPE(e.math1).equals("DOUBLE")) {
-				double left = getDouble(e.math1);
-				double right = getDouble(e.math2);
+				double left = getDouble(e.math1, parameter);
+				double right = getDouble(e.math2, parameter);
 				result = left > right;
 			}
 		}else if(c instanceof Less) {
 			Less e = (Less) c;
 			if(getMATHTYPE(e.math1).equals("INT")) {
-				int left = getInt(e.math1);
-				int right = getInt(e.math2);
+				int left = getInt(e.math1, parameter);
+				int right = getInt(e.math2, parameter);
 				result = left < right;
 			}else if(getMATHTYPE(e.math1).equals("DOUBLE")) {
-				double left = getDouble(e.math1);
-				double right = getDouble(e.math2);
+				double left = getDouble(e.math1, parameter);
+				double right = getDouble(e.math2, parameter);
 				result = left < right;
 			}
 		}else if(c instanceof CondVarName) {
