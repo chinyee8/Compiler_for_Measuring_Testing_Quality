5111f305ad8e2a3a7647d539a673296adc1d427e
chinyee8 <laichinyee1101@gmail.com>
2022-12-05 02:34:13 -0500

    Update Loop.java

--- Project/src/model/Loop.java
+++ Project/src/model/Loop.java
@@ -7,11 +7,13 @@ public class Loop {
 	public int iterationGoal;
 	public List<String> semanticErrors;
 	public HashMap<String, Values> variableMap;
-	public MyMethodBody loopbody;
+	public List<MyMethodBody> loopbody;
+	public MyMethodBody body;
 	
-	public Loop(int iterationGoal, MyMethodBody mb, List<String> semanticErrors, HashMap<String, Values> variableMap) {
+	public Loop(int iterationGoal, List<MyMethodBody> mb, MyMethodBody mb2, List<String> semanticErrors, HashMap<String, Values> variableMap) {
 		this.iterationGoal = iterationGoal;
 		this.loopbody = mb;
+		this.body = mb2;
 		this.semanticErrors = semanticErrors;
 		this.variableMap = variableMap;
 	}
