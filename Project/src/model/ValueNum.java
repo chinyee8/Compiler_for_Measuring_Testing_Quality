1d1e4c7bb7c1aea5f0d43fb408d7d2da2b5d50c9
chinyee8 <laichinyee1101@gmail.com>
2022-12-05 20:27:12 -0500

    Update ValueNum.java

--- Project/src/model/ValueNum.java
+++ Project/src/model/ValueNum.java
@@ -2,7 +2,7 @@ package model;
 
 public class ValueNum extends Values{
 // should I make each containers for NUM | DOUBLE | STRING | CHAR | BOOL ?? 
-	int num;
+	public int num;
 	public ValueNum(int num) {
 		this.num = num;
 	}
